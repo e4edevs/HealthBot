4d9f96dce51956d59a092f4a4dd625a7ca590e85
Ini-mfon <Ini-mfon@VERNI>
2017-04-13 16:11:57 +0100

    Catching database errors

--- Bot Application1/datacon.cs
+++ Bot Application1/datacon.cs
@@ -16,39 +16,53 @@ namespace HealthBot
 
         public datacon()
         {
-            var con = new MySqlConnection("Server=localhost;Database=healthbot;Uid=root;Password=;Connection Timeout = 15");
-            con.Open();
-            con.Close();
+            try
+            {
+                var con = new MySqlConnection("Server=localhost;Database=healthbot;Uid=root;Password=;Connection Timeout = 15");
+                con.Open();
+                con.Close();
 
-            string sql = "select * from ailments";
-            ada = new MySqlDataAdapter(sql, con);
-            ada.Fill(ds, "ailments");
+                string sql = "select * from ailments";
+                ada = new MySqlDataAdapter(sql, con);
+                ada.Fill(ds, "ailments");
 
-            sql = "select * from foods";
-            fda = new MySqlDataAdapter(sql, con);
-            fda.Fill(ds, "foods");
+                sql = "select * from foods";
+                fda = new MySqlDataAdapter(sql, con);
+                fda.Fill(ds, "foods");
 
-            sql = "select * from advice";
-            adda = new MySqlDataAdapter(sql, con);
-            adda.Fill(ds, "advice");
+                sql = "select * from advice";
+                adda = new MySqlDataAdapter(sql, con);
+                adda.Fill(ds, "advice");
 
-            sql = "select * from logs";
-            lda = new MySqlDataAdapter(sql, con);
-            lda.Fill(ds, "logs");
+                sql = "select * from logs";
+                lda = new MySqlDataAdapter(sql, con);
+                lda.Fill(ds, "logs");
 
-            nm = "Connection established";
+                nm = "Connection established";
+            }
+            catch (Exception)
+            {
+
+            }
         }
 
         public void savequery(string query, string intent, string reply)
         {
-            MySqlCommandBuilder cb = new MySqlCommandBuilder(lda);
-            DataRow dr = ds.Tables["logs"].NewRow();
-            dr["chatQuery"] = query;
-            dr["chatIntent"] = intent;
-            dr["chatReply"] = reply;
-            dr["timestamp"] = DateTime.Now;
-            ds.Tables["logs"].Rows.Add(dr);
-            lda.Update(ds, "logs");
+            try
+            {
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(lda);
+                DataRow dr = ds.Tables["logs"].NewRow();
+                dr["chatQuery"] = query;
+                dr["chatIntent"] = intent;
+                dr["chatReply"] = reply;
+                dr["timestamp"] = DateTime.Now;
+                ds.Tables["logs"].Rows.Add(dr);
+                lda.Update(ds, "logs");
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
